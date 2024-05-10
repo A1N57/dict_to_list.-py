55f4eaf169de8a28785127cda3f8e52b1ab5f56c
alexei <lexa_nosov99@mail.ru>
2024-05-10 19:34:16 +0500

    creat new method

--- filter_list.py
+++ filter_list.py
@@ -1,8 +1,6 @@
 def filter_list(my_list, type1):
-    new_list = []
-    for el in my_list:
-        if type(el) == type1:
-            new_list.append(el)
-    return(new_list)
+    def check_element(elem):
+        return isinstance(elem, type1)
+    return list(filter(check_element, my_list))
 
 print(filter_list([1, True, 'as', 5], int))
