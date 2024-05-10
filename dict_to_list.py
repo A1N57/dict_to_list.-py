9a6058acb6ff260674d662ea76cbf6afdba9e871
alexei <lexa_nosov99@mail.ru>
2024-05-10 19:03:46 +0500

    Creat def dict_to_list

---
+++ dict_to_list.py
@@ -0,0 +1,9 @@
+def dict_to_list(a):
+    list = []
+    for k, v in a.items():
+      if type(v) == int:
+        v *= 2
+        list.append((k, v))    
+    return list
+print(dict_to_list({'f': 2, 'r': 3}))
+  
---
+++ main.py
@@ -0,0 +1,7 @@
+n = 123
+n1 = n // 100
+print(n1)
+n2 = (n % 100) // 10
+print(n2)
+n3 = n % 10
+print(n3)
