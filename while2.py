d90ef832898700299e2e9919499baf99bee006be
alexei <lexa_nosov99@mail.ru>
2024-05-10 22:51:34 +0500

    add try except

--- while2.py
+++ while2.py
@@ -1,6 +1,12 @@
 while True:
-    num1 = float(input("Enter first num: "))
-    num2 = float(input("Enter second num: "))
+    try:
+        num1 = float(input("Enter first num: "))
+        num2 = float(input("Enter second num: "))
+    except ValueError as e:
+        print(e)
+        print("You must enter numbers!")
+        continue
+    
     print(num1 / num2)
     answer = input("Do you want continue yes or no? ")
     if answer == 'no':
