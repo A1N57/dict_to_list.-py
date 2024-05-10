8bea06bd9e84a2306563172b60968442b8435a4b
alexei <lexa_nosov99@mail.ru>
2024-05-10 22:43:23 +0500

    creat while2

---
+++ while2.py
@@ -0,0 +1,9 @@
+while True:
+    num1 = float(input("Enter first num: "))
+    num2 = float(input("Enter second num: "))
+    print(num1 / num2)
+    answer = input("Do you want continue yes or no? ")
+    if answer == 'no':
+        break
+
+   
